9bafe4e76022e6f32520c870d2ca5d82016bf952
David Chambers <dc@davidchambers.me>
2021-10-15 15:26:25 +0200

    Ice Age I

--- src/data/events.js
+++ src/data/events.js
@@ -6,4 +6,21 @@ import {Unplayed, Drop, Played} from '../adt/Match.js';
 const {Nothing, Just, Left, Right, Pair} = S;
 
 export default [
+  {
+    id: 1,
+    name: 'Ice Age I',
+    matches: [
+      Played (Right (Pair (1) (2))),
+      Played (Left (Pair (3) (4))),
+      Played (Left (Pair (5) (6))),
+      Played (Right (Pair (3) (5))),
+      Played (Left (Pair (2) (5))),
+      Played (Right (Pair (1) (3))),
+      Played (Left (Pair (4) (6))),
+      Played (Left (Pair (3) (4))),
+      Played (Left (Pair (5) (3))),
+      Played (Left (Pair (2) (5))),
+      Unplayed (Pair (Nothing) (Just (2))),
+    ],
+  },
 ];
--- src/data/players.js
+++ src/data/players.js
@@ -4,4 +4,40 @@ import S from 'sanctuary';
 const {Nothing, Just} = S;
 
 export default [
+  {
+    id: 1,
+    name: 'Bryan Manolakos',
+    country: Just ('US'),
+    tz: Just ('America/New_York'),
+  },
+  {
+    id: 2,
+    name: 'William Larson',
+    country: Just ('US'),
+    tz: Just ('America/New_York'),
+  },
+  {
+    id: 3,
+    name: 'David Chambers',
+    country: Just ('NZ'),
+    tz: Just ('Europe/Berlin'),
+  },
+  {
+    id: 4,
+    name: 'Richard Stebbing',
+    country: Just ('GB'),
+    tz: Just ('Europe/London'),
+  },
+  {
+    id: 5,
+    name: 'Dave Firth Bard',
+    country: Just ('US'),
+    tz: Just ('America/New_York'),
+  },
+  {
+    id: 6,
+    name: 'Scott Bradley',
+    country: Just ('US'),
+    tz: Just ('America/New_York'),
+  },
 ];
